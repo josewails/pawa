29fb0309565d564de17d3577c16c42ece5b0741a
josewails <joseritch45@gmail.com>
2018-07-19 08:29:51 +0300

    added SITE_URL

--- pawa/settings/production.py
+++ pawa/settings/production.py
@@ -11,3 +11,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+
+SITE_URL = 'https://pamojaness.herokuapp.com'
