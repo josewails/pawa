4d422ba444fcdb5cf51c30e8de2f872dcdf56036
josewails <joseritch45@gmail.com>
2018-07-22 21:00:56 +0300

    change site-url to pamojaness

--- pawa/settings/production.py
+++ pawa/settings/production.py
@@ -12,4 +12,4 @@ DATABASES['default'].update(db_from_env)
 
 PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 
-SITE_URL = 'https://pamojaness.herokuapp.com'
+SITE_URL = 'https://pawaness.herokuapp.com'
